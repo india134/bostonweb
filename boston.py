1bd63cf8b1b40ee7b696e2ed77cc32e1b9ff77a6
india134 <138314543+india134@users.noreply.github.com>
2023-08-31 08:18:58 +0530

    Update boston.py

--- boston.py
+++ boston.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,r2_score
 from sklearn.linear_model import LogisticRegression
